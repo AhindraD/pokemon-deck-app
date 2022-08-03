968083c270a4d6550400ed0415c5fe266798583d
Ahindra D <official.ahindra@gmail.com>
2022-08-03 14:42:42 +0530

    logo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,10 @@ export default function Card(props) {
         fetch(props.url)
             .then(response => response.json())
             .then(data => setPokemon(data))
-            //https://pokeapi.co/api/v2/pokemon/1/
+        //https://pokeapi.co/api/v2/pokemon/1/
+        //pokemon.sprites.other["official-artwork"].front_default
+        //pokemon.sprites.other.home['front_default']
+
     }, [])
     return (
         <>
@@ -14,7 +17,7 @@ export default function Card(props) {
                 <div className='card'>
                     <p className="pokemon-name">{pokemon.name}</p>
                     <div className="pokemon-image">
-                        <img src={pokemon.sprites.other.home['front_default']} alt="" />
+                        <img src={pokemon.sprites.other["official-artwork"].front_default} alt="" />
                     </div>
                     <p className="pokemon-type">{pokemon.types[0].type.name}</p>
                 </div>
